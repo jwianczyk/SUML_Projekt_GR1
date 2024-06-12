add9a98dcc058af289f54bd5845c04cb47323c44
Jakub Wiańczyk <jwianczyk@gmail.com>
2024-06-12 23:32:25 +0200

    Tmp commit 12.06

--- models/scoring_models.py
+++ models/scoring_models.py
@@ -1,7 +1,8 @@
 import torch.nn as nn
+from pydantic import BaseModel
 
 
-class WritingScoreModel(nn.Module):
+class WritingScoreModel(nn.Module, BaseModel):
     def __init__(self, input_size, output_size):
         super(WritingScoreModel, self).__init__()
         self.linear1 = nn.Linear(input_size, 8)
@@ -41,3 +42,5 @@ class MathScoreModel(nn.Module):
         x = self.relu1(x)
         x = self.linear2(x)
         return x
+
+
